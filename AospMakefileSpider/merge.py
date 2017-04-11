de01b0e1fa6c9d8e8a1055ae2d66a8fc31220120
tagorewang <wtao901231@gmail.com>
2017-04-11 14:39:56 +0800

    fix bad json format

--- AospMakefileSpider/merge.py
+++ AospMakefileSpider/merge.py
@@ -15,7 +15,11 @@ rev_len = len(rev)
 
 url2localPathMap = {}
 
-for i in {1, 2}:
+mkcnt = 0
+bpcnt = 0
+gradlecnt = 0
+
+for i in {1, 2, 3, 4}:
 	mkfilename = 'aosp_git_mk.json.part%d' % i
 	#print('merge %s' % mkfilename)
 	with open(mkfilename) as mkfile:
@@ -23,6 +27,13 @@ for i in {1, 2}:
 		for ln in mkfileLns:
 			mkfile = json.loads(ln)
 			url = mkfile['url']
+			name = mkfile['name']
+			if '.mk' in name:
+				mkcnt = mkcnt+1
+			elif '.bp' in name:
+				bpcnt = bpcnt+1
+			elif '.gradle' in name:
+				gradlecnt = gradlecnt+1
 			idxOfPlus = url.index('+')
 			gitname = url[prefix_len:idxOfPlus-1]
 			path = url[idxOfPlus+1+rev_len:]
@@ -30,9 +41,14 @@ for i in {1, 2}:
 			path = localDir + path
 			url2localPathMap[url] = path
 
+# print('all count: %d' % (mkcnt + bpcnt + gradlecnt))
+# print('mk count: %d' % mkcnt)
+# print('bp count: %d' % bpcnt)
+# print('gradle count: %d' % gradlecnt)
+
 url2localPathMap = collections.OrderedDict(sorted(url2localPathMap.items()))
 
 print('[')
 for k in url2localPathMap:
-	print('{\'base64-content-download-url\':\'%s?format=TEXT\', \'saved_path\':\'%s},' % (k, url2localPathMap[k]))
+	print('{\"base64-content-download-url\":\"%s?format=TEXT\", \"saved_path\":\"%s\"},' % (k, url2localPathMap[k]))
 print(']')
